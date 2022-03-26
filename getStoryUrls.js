3564e0ffe87e616f8d34dcb4d05b2b2b17e2e83b
Brent <bgrossman@gti.energy>
2022-03-26 15:11:29 -0500

    Initial commit of audio player files

---
+++ getStoryUrls.js
@@ -0,0 +1,36 @@
+const queryUrl =
+    "http://api.npr.org/query?fields=title,audio&date=current&dateType=story&sort=assigned&output=json&numResults=30&apiKey=MDA5NDM4NDA3MDEzMzY1MTMyNjA2NjIyMg001&id=";
+
+function getStoryUrls(showId) {
+    track_list = [];
+    track_index = 0;
+    const url = queryUrl + showId;
+
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+
+    fetch(url, requestOptions)
+        .then(response => response.text())
+        .then(storyData => {
+            // console.log(storyData);
+            parseStoryInfo(storyData);
+        })
+        .catch(error => console.log('error', error));
+}
+
+function parseStoryInfo(storyData) {
+    console.log(JSON.parse(storyData).list);
+    storyDataJson = JSON.parse(storyData);
+    const stories = storyDataJson.list.story;
+    for (story of stories) {
+        let trackObject = {
+            name: story.title.$text,
+            path: story.audio[0].format.mp4.$text
+        }
+        track_list.push(trackObject);
+    }
+    console.log(track_list);
+    loadTrack(track_index);
+}
